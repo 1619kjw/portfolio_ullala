10c1a3bbdcbac61c1d26c309f64e27536032703d
jiwon <kjw89716@gmail.com>
2025-08-04 19:23:29 +0900

    띠배너

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+//띠배너
+const lineBnr = document.querySelector('.line_banner');
+const lineClose = document.querySelector('.line_banner .closeBtn');
+lineClose.addEventListener('click',()=>{
+    lineBnr.style.display = 'none';
+})
