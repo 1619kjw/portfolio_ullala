a276b8799ad47ab4a705ae5c360863cb32cb8ba8
jiwon <kjw89716@gmail.com>
2025-08-15 21:53:38 +0900

    이미지경로수정

--- scripts/ullalaDB.js
+++ scripts/ullalaDB.js
@@ -2,213 +2,213 @@
 
 const newDB = [
     {
-        src:'../images/row2/tap1/01.png',
+        src:'./images/row2/tap1/01.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185WPSSN] 테일러 반팔 여성페어 크림 (32수)',
         price:76000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/02.png',
+        src:'./images/row2/tap1/02.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/03.png',
+        src:'./images/row2/tap1/03.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/04.png',
+        src:'./images/row2/tap1/04.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/01.png',
+        src:'./images/row2/tap1/01.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/01.png',
+        src:'./images/row2/tap1/01.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/01.png',
+        src:'./images/row2/tap1/01.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row2/tap1/01.png',
+        src:'./images/row2/tap1/01.png',
         tags:['NEW','커플','COUPON'],
         brand:'ULLALA',
         name:'[25185MPSSN] 테일러 반팔 남성페어 크림 (32수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
 ]
 
 const styleDB = [
     {
-        src:'../images/row3/tap1/01.png',
+        src:'./images/row3/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row3/tap1/02.png',
+        src:'./images/row3/tap1/02.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row3/tap1/01.png',
+        src:'./images/row3/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row3/tap1/01.png',
+        src:'./images/row3/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
 ]
 
 const bestDB = [
     {
-        src:'../images/row4/tap1/01.png',
+        src:'./images/row4/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/02.png',
+        src:'./images/row4/tap1/02.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/01.png',
+        src:'./images/row4/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/01.png',
+        src:'./images/row4/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/01.png',
+        src:'./images/row4/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/02.png',
+        src:'./images/row4/tap1/02.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/01.png',
+        src:'./images/row4/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
     {
-        src:'../images/row4/tap1/01.png',
+        src:'./images/row4/tap1/01.png',
         tags:['NEW','COUPON'],
         brand:'ULLALA',
         name:'[25156WTSSN] 메르 반팔 투피스 크림 (40수)',
         price:78000,
-        cart:'../images/small_cart.png',
+        cart:'./images/small_cart.png',
     },
 ]
 
 const reviewDB = [
     {
-        src1:'../images/row5/01.png',
-        src2:'../images/row5/02.png'
+        src1:'./images/row5/01.png',
+        src2:'./images/row5/02.png'
     },
     {
-        src1:'../images/row5/03.png',
-        src2:'../images/row5/04.png'
+        src1:'./images/row5/03.png',
+        src2:'./images/row5/04.png'
     },
     {
-        src1:'../images/row5/05.png',
-        src2:'../images/row5/06.png'
+        src1:'./images/row5/05.png',
+        src2:'./images/row5/06.png'
     },
     {
-        src1:'../images/row5/07.png',
-        src2:'../images/row5/08.png'
+        src1:'./images/row5/07.png',
+        src2:'./images/row5/08.png'
     },
     {
-        src1:'../images/row5/09.png',
-        src2:'../images/row5/10.png'
+        src1:'./images/row5/09.png',
+        src2:'./images/row5/10.png'
     },
     {
-        src1:'../images/row5/11.png',
-        src2:'../images/row5/12.png'
+        src1:'./images/row5/11.png',
+        src2:'./images/row5/12.png'
     },
     {
-        src1:'../images/row5/13.png',
-        src2:'../images/row5/14.png'
+        src1:'./images/row5/13.png',
+        src2:'./images/row5/14.png'
     },
     {
-        src1:'../images/row5/09.png',
-        src2:'../images/row5/10.png'
+        src1:'./images/row5/09.png',
+        src2:'./images/row5/10.png'
     },
     {
-        src1:'../images/row5/11.png',
-        src2:'../images/row5/12.png'
+        src1:'./images/row5/11.png',
+        src2:'./images/row5/12.png'
     },
     {
-        src1:'../images/row5/13.png',
-        src2:'../images/row5/14.png'
+        src1:'./images/row5/13.png',
+        src2:'./images/row5/14.png'
     },
 ]
 
